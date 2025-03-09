39fc28342b8ed60258bd06d18d7d2489d860c98e
Steve Blum <steve.blum@live.de>
2025-03-09 23:10:47 +0100

    feat: added support for rust

--- files/nvim/lua/plugins/treesitter.lua
+++ files/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "markdown" },
+			ensure_installed = { "lua", "javascript", "markdown", "rust"},
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
