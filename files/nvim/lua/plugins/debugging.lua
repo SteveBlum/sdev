ad0f6a5d74e1770f33ed8b5f573a4380d8cbf4e6
Steve Blum <steve.blum@live.de>
2025-07-26 20:07:11 +0200

    fix: added missing debugging keybindings and profiles
    * fix: LSPs run twice
    
    * fix: added missing DAP keymaps
    
    * feat: additional debugging keymaps and profiles

--- files/nvim/lua/plugins/debugging.lua
+++ files/nvim/lua/plugins/debugging.lua
@@ -15,12 +15,8 @@ return {
     dap.listeners.before.launch.dapui_config = function()
       dapui.open()
     end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    dap.listeners.before.event_terminated.dapui_config = function() end
+    dap.listeners.before.event_exited.dapui_config = function() end
 
     local function get_pkg_path(pkg, path)
       pcall(require, "mason")
@@ -54,29 +50,12 @@ return {
       {
         type = "pwa-node",
         request = "launch",
-        name = "Launch TS file",
-        program = "${file}",
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = "inspector",
-      },
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch Current File (pwa-node)",
-        cwd = vim.fn.getcwd(),
-        args = { "${file}" },
-        sourceMaps = true,
-        protocol = "inspector",
-      },
-      {
-        type = "pwa-node",
-        request = "launch",
         name = "Launch Current File (pwa-node with ts-node)",
-        cwd = vim.fn.getcwd(),
-        runtimeArgs = { "--loader", "ts-node/esm" },
+        runtimeArgs = { "-r", "ts-node/register" },
         runtimeExecutable = "node",
-        args = { "${file}" },
+        args = { "--inspect", "${file}" },
+        rootPath = "${workspaceFolder}",
+        cwd = "${workspaceFolder}",
         sourceMaps = true,
         protocol = "inspector",
         skipFiles = { "<node_internals>/**", "node_modules/**" },
@@ -99,12 +78,110 @@ return {
         internalConsoleOptions = "neverOpen",
         skipFiles = { "<node_internals>/**", "node_modules/**" },
       },
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch Test Current File (pwa-node with mocha)",
+        runtimeArgs = { "${workspaceFolder}/node_modules/.bin/mocha" },
+        runtimeExecutable = "node",
+        cwd = vim.fn.getcwd(),
+        args = { "${file}", "--coverage", "false" },
+        rootPath = "${workspaceFolder}",
+        sourceMaps = true,
+        console = "integratedTerminal",
+        internalConsoleOptions = "neverOpen",
+        skipFiles = { "<node_internals>/**", "node_modules/**" },
+      },
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch npm run start:dev",
+        runtimeExecutable = "npm",
+        args = { "run", "start:dev" },
+        rootPath = "${workspaceFolder}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        console = "integratedTerminal",
+        internalConsoleOptions = "neverOpen",
+        protocol = "inspector",
+        skipFiles = { "<node_internals>/**", "node_modules/**" },
+        outFiles = { "${workspaceFolder}/dist/**/*.js" },
+        resolveSourceMapLocations = {
+          "${workspaceFolder}/**",
+          "!**/node_modules/**",
+        },
+      },
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch npm run start:debug",
+        runtimeExecutable = "npm",
+        args = { "run", "start:debug" },
+        rootPath = "${workspaceFolder}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        console = "integratedTerminal",
+        internalConsoleOptions = "neverOpen",
+        protocol = "inspector",
+        skipFiles = { "<node_internals>/**", "node_modules/**" },
+        outFiles = { "${workspaceFolder}/dist/**/*.js" },
+        resolveSourceMapLocations = {
+          "${workspaceFolder}/**",
+          "!**/node_modules/**",
+        },
+      },
     }
-    vim.keymap.set("n", "<Leader>dt", function()
-      dap.set_breakpoint()
+
+    -- DAPUI Keymaps
+    vim.keymap.set("n", "<leader>dq", dapui.close)
+
+    -- DAP Keymaps
+    local function add_tagfunc(widget)
+      local orig_new_buf = widget.new_buf
+      widget.new_buf = function(...)
+        local bufnr = orig_new_buf(...)
+        vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.require'me.lsp.ext'.symbol_tagfunc")
+        return bufnr
+      end
+    end
+
+    local widgets = require("dap.ui.widgets")
+    add_tagfunc(widgets.expression)
+    add_tagfunc(widgets.scopes)
+    local keymap = vim.keymap
+    local function set(mode, lhs, rhs)
+      keymap.set(mode, lhs, rhs, { silent = true })
+    end
+    set({ "n", "t" }, "<F3>", dap.terminate)
+    set({ "n", "t" }, "<F5>", dap.continue)
+    set({ "n", "t" }, "<F10>", dap.step_over)
+    set({ "n", "t" }, "<F11>", dap.step_into)
+    set({ "n", "t" }, "<F12>", dap.step_out)
+    set("n", "<leader>b", dap.toggle_breakpoint)
+    set("n", "<leader>B", function()
+      dap.toggle_breakpoint(vim.fn.input("Breakpoint Condition: "), nil, nil, true)
     end)
-    vim.keymap.set("n", "<Leader>dc", function()
-      dap.continue()
+    set("n", "<leader>lp", function()
+      dap.toggle_breakpoint(nil, nil, vim.fn.input("Log point message: "), true)
     end)
+    set("n", "<leader>dr", function()
+      dap.repl.toggle({ height = 15 })
+    end)
+    set("n", "<leader>dl", dap.run_last)
+    set("n", "<leader>dj", dap.down)
+    set("n", "<leader>dk", dap.up)
+    set("n", "<leader>dc", dap.run_to_cursor)
+    set("n", "<leader>dg", dap.goto_)
+    set("n", "<leader>dS", function()
+      widgets.centered_float(widgets.frames)
+    end)
+    set("n", "<leader>dt", function()
+      widgets.centered_float(widgets.threads)
+    end)
+    set("n", "<leader>ds", function()
+      widgets.centered_float(widgets.scopes)
+    end)
+    set({ "n", "v" }, "<leader>dh", widgets.hover)
+    set({ "n", "v" }, "<leader>dp", widgets.preview)
   end,
 }
--- files/nvim/lua/plugins/lsp-config.lua
+++ files/nvim/lua/plugins/lsp-config.lua
@@ -15,23 +15,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-			lspconfig.vtsls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities
-      })
       lspconfig.helm_ls.setup({
-        capabilities = capabilities,
         settings = {
           ['helm-ls'] = {
             yamlls = {
