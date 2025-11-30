bb06d73e9fa180c22141fceb84b81b574f4add63
Steve Blum <steve.blum@live.de>
2025-11-30 09:16:14 +0100

    feat: added gemini-cli (#286)

--- files/runtime-setup.sh
+++ files/runtime-setup.sh
@@ -3,4 +3,4 @@ npm config set prefix /root/.npm
 echo "export PATH=\"/root/.npm/bin:$PATH\"" > /root/.bashrc.env
 chmod +x /root/.bashrc.env
 pipx install --include-deps neovim
-npm install -g neovim prettier @modelcontextprotocol/server-filesystem mcp-server-commands @charmland/crush
+npm install -g neovim prettier @modelcontextprotocol/server-filesystem mcp-server-commands @charmland/crush @google/gemini-cli
