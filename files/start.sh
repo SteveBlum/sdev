7fb050c5204f519c69a70c300468b1694a51aa69
Steve Blum <steve.blum@live.de>
2025-12-07 15:56:15 +0100

    feat: start goose when sdev starts (#296)

--- files/start.sh
+++ files/start.sh
@@ -3,4 +3,4 @@ git config --global user.name $GIT_USER
 git config --global user.email $GIT_EMAIL
 git config --global user.signingkey $GIT_SIGNINGKEY
 /runtime-setup.sh
-tmux -u
+tmux new-session -d -s my_session -n 'goose' 'goose web --host 0.0.0.0 --port 3010' -u \; new-window -t my_session:2 && tmux attach -t my_session
