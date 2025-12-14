7916ee06446d3bbdef946b9bafe841e6a5b45a04
Steve Blum <steve.blum@live.de>
2025-12-14 23:20:27 +0100

    fix: goose doesn't startup

--- files/start.sh
+++ files/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git config --global user.name $GIT_USER
+git config --global user.name "$GIT_USER"
 git config --global user.email $GIT_EMAIL
 git config --global user.signingkey $GIT_SIGNINGKEY
 /runtime-setup.sh
-tmux new-session -d -s my_session -n 'goose' 'goose web --host 0.0.0.0 --port 3010' -u \; new-window -t my_session:2 && tmux attach -t my_session
+tmux new-session -d -s my_session -n 'goose' 'goose web --host 0.0.0.0 --port 3010' \; new-window -t my_session:1 && tmux attach -t my_session
