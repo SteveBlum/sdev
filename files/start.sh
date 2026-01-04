c346d1b1538f822f3a526ed90ab19ee053f109a0
Steve Blum <steve.blum@live.de>
2026-01-05 00:09:01 +0100

    feat: replaced goose and crush with opencode (#319)

--- files/start.sh
+++ files/start.sh
@@ -4,4 +4,4 @@ git config --global user.email $GIT_EMAIL
 git config --global user.signingkey $GIT_SIGNINGKEY
 /runtime-setup.sh
 /usr/bin/supervisord -c /etc/supervisord.conf &
-tmux new-session -d -s my_session -n 'goose' "tail -F /var/log/goose.out" \; new-window -t my_session:1 && tmux attach -t my_session
+tmux -u
